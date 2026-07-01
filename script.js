26bba33820753ed9f8ad7907676d2d0c47db9300
Rubén <hunterruben4@gmail.com>
2026-06-30 19:08:49 -0500

    actualziar cv

--- script.js
+++ script.js
@@ -75,3 +75,10 @@ function animarTexto() {
 
     setTimeout(animarTexto, velocidad);
 }
+
+const menuToggle = document.getElementById("menu-toggle");
+const navList = document.getElementById("nav-list");
+
+menuToggle.addEventListener("click", () => {
+    navList.classList.toggle("active");
+});
